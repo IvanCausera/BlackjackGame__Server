07a715e2e93890300a92d0f1c4de214aa1e02054
fsevaga <fsevaga@gmail.com>
2022-02-03 19:02:44 +0100

    Clase Player hecha pero no probada

--- src/threads/Player.java
+++ src/threads/Player.java
@@ -1,5 +1,66 @@
 package threads;
 
-public class Player {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import tools.Tools;
+
+public class Player implements Runnable {
+
+	@Override
+	public void run() {
+		try {
+			Socket s = new Socket(InetAddress.getByName("localhost"),2001);	
+			BufferedWriter exit = new BufferedWriter (new OutputStreamWriter (s.getOutputStream ()));
+			BufferedReader entry = new BufferedReader (new InputStreamReader (s.getInputStream ()));
+			
+			System.out.println(entry.readLine());
+			int c = 0;
+			String msgReturn;
+			do {
+				sendMessage(exit, String.valueOf(Tools.randomNumber(1, 750)));
+				msgReturn = entry.readLine();
+				System.out.println(msgReturn);
+			} while (!msgReturn.contains("Ok"));
+			
+			
+			for (int i = 0; i < 2; i++) {
+				msgReturn = entry.readLine();
+				while (msgReturn.contains("?")) {
+					sendMessage(exit, String.valueOf(Tools.randomNumber(1, 11)));
+				}
+			}
+			System.out.println(entry.readLine());
+			
+			boolean gameOver = false;
+			while (!gameOver) {
+				msgReturn = entry.readLine();
+				System.out.println(msgReturn);
+				while (msgReturn.contains("?")) {
+					sendMessage(exit, String.valueOf(Tools.randomNumber(1, 11)));
+				}
+				System.out.println(entry.readLine());
+			}
+			
+		} catch (SocketException e) {
+			e.printStackTrace(); 
+		}catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void sendMessage(BufferedWriter exit, String msg) throws IOException {
+		exit.write(msg + System.lineSeparator());
+		exit.flush();
+	}
 
 }
