abb698b18fbef56400f5664843511d307e845466
fsevaga <fsevaga@gmail.com>
2022-02-10 19:49:43 +0100

    Creada una clase en el paquete test en el que se incluyen los Strings de las cartas

---
+++ src/test/CartasString.java
@@ -0,0 +1,649 @@
+package test;
+
+public class CartasString {
+
+	public static void main(String[] args) {
+		// "Club -> \u2663"
+		// "Spades -> \u2660"
+		// "Diamond -> \u2666"
+		// "Heart -> \u2665"
+		
+		System.out.println(
+				  "ACE OF CLUBS\n"
+				+ " _____\n"
+				+ "|A    |\n"
+				+ "|     |\n"
+				+ "|  \u2663  |\n"
+				+ "|     |\n"
+				+ "|____V| ");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println( 
+				   "TWO OF CLUBS \n"
+				+  " _____ \n"
+				+ "|2    |\n"
+				+ "|  \u2663  |\n"
+				+ "|     |\n"
+				+ "|  \u2663  |\n"
+				+ "|____Z|\n");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "THREE OF CLUBS\n"
+				+ " _____\n"
+				+ "|3    |\n"
+				+ "|  \u2663  |\n"
+				+ "|  \u2663  |\n"
+				+ "|  \u2663  |\n"
+				+ "|____E| ");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "FOUR OF CLUBS\n"
+				+ " _____ |\n"
+				+ "|4    |\n"
+				+ "| \u2663 \u2663 |\n"
+				+ "|     |\n"
+				+ "| \u2663 \u2663 |\n"
+				+ "|____h|");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "FIVE OF CLUBS\n"
+				+ " _____\n"
+				+ "|5    |\n"
+				+ "| \u2663 \u2663 |\n"
+				+ "|  \u2663  |\n"
+				+ "| \u2663 \u2663 |\n"
+				+ "|____S|");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "SIX OF CLUBS\n"
+				+ " _____\n"
+				+ "|6    |\n"
+				+ "| \u2663 \u2663 |\n"
+				+ "| \u2663 \u2663 |\n"
+				+ "| \u2663 \u2663 |\n"
+				+ "|____9|");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "SEVEN OF CLUBS\n"
+				+ " _____\n"
+				+ "|7    |\n"
+				+ "| \u2663 \u2663 |\n"
+				+ "|\u2663 \u2663 \u2663|\n"
+				+ "| \u2663 \u2663 |\n"
+				+ "|____L|");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "EIGHT OF CLUBS\n"
+				+ " _____\n"
+				+ "|8    |\n"
+				+ "|\u2663 \u2663 \u2663|\n"
+				+ "| \u2663 \u2663 |\n"
+				+ "|\u2663 \u2663 \u2663|\n"
+				+ "|____8|");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "NINE OF CLUBS\n"
+				+ " _____\n"
+				+ "|9    |\n"
+				+ "|\u2663 \u2663 \u2663|\n"
+				+ "|\u2663 \u2663 \u2663|\n"
+				+ "|\u2663 \u2663 \u2663|\n"
+				+ "|____6|");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "TEN OF CLUBS\n"
+				+ " _____\n"
+				+ "|10 \u2663 |\n"
+				+ "|\u2663 \u2663 \u2663|\n"
+				+ "|\u2663 \u2663 \u2663|\n"
+				+ "|\u2663 \u2663 \u2663|\n"
+				+ "|____0I|");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "JACK OF CLUBS\n"
+				+ " _____\n"
+				+ "|J    |\n"
+				+ "|     |\n"
+				+ "|  \u2663  |\n"
+				+ "|     |\n"
+				+ "|_____| ");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "QUEEN OF CLUBS\n"
+				+ " _____\n"
+				+ "|Q    |\n"
+				+ "|     |\n"
+				+ "|  \u2663  |\n"
+				+ "|     |\n"
+				+ "|_____| ");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "KING OF CLUBS\n"
+				+ " _____\n"
+				+ "|K    |\n"
+				+ "|     |\n"
+				+ "|  \u2663  |\n"
+				+ "|     |\n"
+				+ "|_____| ");
+		
+		System.out.println("\n-----------------------------------------------------------------\n");
+		
+		// "Club -> \u2663"
+		// "Spades -> \u2660"
+		// "Diamond -> \u2666"
+		// "Heart -> \u2665"
+		
+		System.out.println(
+				  "ACE OF SPADES\n"
+				+ " _____\n"
+				+ "|A    |\n"
+				+ "|     |\n"
+				+ "|  \u2660  |\n"
+				+ "|     |\n"
+				+ "|____V| ");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println( 
+				   "TWO OF SPADES \n"
+				+  " _____ \n"
+				+ "|2    |\n"
+				+ "|  \u2660  |\n"
+				+ "|     |\n"
+				+ "|  \u2660  |\n"
+				+ "|____Z|\n");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "THREE OF SPADES\n"
+				+ " _____\n"
+				+ "|3    |\n"
+				+ "|  \u2660  |\n"
+				+ "|  \u2660  |\n"
+				+ "|  \u2660  |\n"
+				+ "|____E| ");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "FOUR OF SPADES\n"
+				+ " _____ |\n"
+				+ "|4    |\n"
+				+ "| \u2660 \u2660 |\n"
+				+ "|     |\n"
+				+ "| \u2660 \u2660 |\n"
+				+ "|____h|");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "FIVE OF SPADES\n"
+				+ " _____\n"
+				+ "|5    |\n"
+				+ "| \u2660 \u2660 |\n"
+				+ "|  \u2660  |\n"
+				+ "| \u2660 \u2660 |\n"
+				+ "|____S|");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "SIX OF SPADES\n"
+				+ " _____\n"
+				+ "|6    |\n"
+				+ "| \u2660 \u2660 |\n"
+				+ "| \u2660 \u2660 |\n"
+				+ "| \u2660 \u2660 |\n"
+				+ "|____9|");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "SEVEN OF SPADES\n"
+				+ " _____\n"
+				+ "|7    |\n"
+				+ "| \u2660 \u2660 |\n"
+				+ "|\u2660 \u2660 \u2663|\n"
+				+ "| \u2660 \u2660 |\n"
+				+ "|____L|");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "EIGHT OF SPADES\n"
+				+ " _____\n"
+				+ "|8    |\n"
+				+ "|\u2660 \u2660 \u2660|\n"
+				+ "| \u2660 \u2660 |\n"
+				+ "|\u2660 \u2660 \u2660|\n"
+				+ "|____8|");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "NINE OF SPADES\n"
+				+ " _____\n"
+				+ "|9    |\n"
+				+ "|\u2660 \u2660 \u2660|\n"
+				+ "|\u2660 \u2660 \u2660|\n"
+				+ "|\u2660 \u2660 \u2660|\n"
+				+ "|____6|");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "TEN OF SPADES\n"
+				+ " _____\n"
+				+ "|10 \u2660 |\n"
+				+ "|\u2660 \u2660 \u2660|\n"
+				+ "|\u2660 \u2660 \u2660|\n"
+				+ "|\u2660 \u2660 \u2660|\n"
+				+ "|____0I|");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "JACK OF SPADES\n"
+				+ " _____\n"
+				+ "|J    |\n"
+				+ "|     |\n"
+				+ "|  \u2660  |\n"
+				+ "|     |\n"
+				+ "|_____| ");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "QUEEN OF SPADES\n"
+				+ " _____\n"
+				+ "|Q    |\n"
+				+ "|     |\n"
+				+ "|  \u2660  |\n"
+				+ "|     |\n"
+				+ "|_____| ");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "KING OF SPADES\n"
+				+ " _____\n"
+				+ "|K    |\n"
+				+ "|     |\n"
+				+ "|  \u2660  |\n"
+				+ "|     |\n"
+				+ "|_____| ");
+		
+		System.out.println("\n-----------------------------------------------------------------\n");
+		
+		
+		// "Club -> \u2663"
+		// "Spades -> \u2660"
+		// "Diamond -> \u2666"
+		// "Heart -> \u2665"
+		
+		System.out.println(
+				  "ACE OF DIAMONDS\n"
+				+ " _____\n"
+				+ "|A    |\n"
+				+ "|     |\n"
+				+ "|  \u2666  |\n"
+				+ "|     |\n"
+				+ "|____V| ");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println( 
+				   "TWO OF DIAMONDS \n"
+				+  " _____ \n"
+				+ "|2    |\n"
+				+ "|  \u2666  |\n"
+				+ "|     |\n"
+				+ "|  \u2666  |\n"
+				+ "|____Z|\n");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "THREE OF DIAMONDS\n"
+				+ " _____\n"
+				+ "|3    |\n"
+				+ "|  \u2666  |\n"
+				+ "|  \u2666  |\n"
+				+ "|  \u2666  |\n"
+				+ "|____E| ");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "FOUR OF DIAMONDS\n"
+				+ " _____ |\n"
+				+ "|4    |\n"
+				+ "| \u2666 \u2666 |\n"
+				+ "|     |\n"
+				+ "| \u2666 \u2666 |\n"
+				+ "|____h|");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "FIVE OF DIAMONDS\n"
+				+ " _____\n"
+				+ "|5    |\n"
+				+ "| \u2666 \u2666 |\n"
+				+ "|  \u2666  |\n"
+				+ "| \u2666 \u2666 |\n"
+				+ "|____S|");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "SIX OF DIAMONDS\n"
+				+ " _____\n"
+				+ "|6    |\n"
+				+ "| \u2666 \u2666 |\n"
+				+ "| \u2666 \u2666 |\n"
+				+ "| \u2666 \u2666 |\n"
+				+ "|____9|");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "SEVEN OF DIAMONDS\n"
+				+ " _____\n"
+				+ "|7    |\n"
+				+ "| \u2666 \u2666 |\n"
+				+ "|\u2666 \u2666 \u2666|\n"
+				+ "| \u2666 \u2666 |\n"
+				+ "|____L|");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "EIGHT OF DIAMONDS\n"
+				+ " _____\n"
+				+ "|8    |\n"
+				+ "|\u2666 \u2666 \u2666|\n"
+				+ "| \u2666 \u2666 |\n"
+				+ "|\u2666 \u2666 \u2666|\n"
+				+ "|____8|");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "NINE OF DIAMONDS\n"
+				+ " _____\n"
+				+ "|9    |\n"
+				+ "|\u2666 \u2666 \u2666|\n"
+				+ "|\u2666 \u2666 \u2666|\n"
+				+ "|\u2666 \u2666 \u2666|\n"
+				+ "|____6|");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "TEN OF DIAMONDS\n"
+				+ " _____\n"
+				+ "|10 \u2666 |\n"
+				+ "|\u2666 \u2666 \u2666|\n"
+				+ "|\u2666 \u2666 \u2666|\n"
+				+ "|\u2666 \u2666 \u2666|\n"
+				+ "|____0I|");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "JACK OF DIAMONDS\n"
+				+ " _____\n"
+				+ "|J    |\n"
+				+ "|     |\n"
+				+ "|  \u2666  |\n"
+				+ "|     |\n"
+				+ "|_____| ");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "QUEEN OF DIAMONDS\n"
+				+ " _____\n"
+				+ "|Q    |\n"
+				+ "|     |\n"
+				+ "|  \u2666  |\n"
+				+ "|     |\n"
+				+ "|_____| ");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "KING OF DIAMONDS\n"
+				+ " _____\n"
+				+ "|K    |\n"
+				+ "|     |\n"
+				+ "|  \u2666  |\n"
+				+ "|     |\n"
+				+ "|_____| ");
+		
+		System.out.println("\n-----------------------------------------------------------------\n");
+		
+		
+		// "Club -> \u2663"
+		// "Spades -> \u2660"
+		// "Diamond -> \u2666"
+		// "Heart -> \u2665"
+		
+		System.out.println(
+				  "ACE OF HEARTS\n"
+				+ " _____\n"
+				+ "|A    |\n"
+				+ "|     |\n"
+				+ "|  \u2665  |\n"
+				+ "|     |\n"
+				+ "|____V| ");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println( 
+				   "TWO OF HEARTS \n"
+				+  " _____ \n"
+				+ "|2    |\n"
+				+ "|  \u2665  |\n"
+				+ "|     |\n"
+				+ "|  \u2665  |\n"
+				+ "|____Z|\n");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "THREE OF HEARTS\n"
+				+ " _____\n"
+				+ "|3    |\n"
+				+ "|  \u2665  |\n"
+				+ "|  \u2665  |\n"
+				+ "|  \u2665  |\n"
+				+ "|____E| ");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "FOUR OF HEARTS\n"
+				+ " _____ |\n"
+				+ "|4    |\n"
+				+ "| \u2665 \u2665 |\n"
+				+ "|     |\n"
+				+ "| \u2665 \u2665 |\n"
+				+ "|____h|");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "FIVE OF HEARTS\n"
+				+ " _____\n"
+				+ "|5    |\n"
+				+ "| \u2665 \u2665 |\n"
+				+ "|  \u2665  |\n"
+				+ "| \u2665 \u2665 |\n"
+				+ "|____S|");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "SIX OF HEARTS\n"
+				+ " _____\n"
+				+ "|6    |\n"
+				+ "| \u2665 \u2665 |\n"
+				+ "| \u2665 \u2665 |\n"
+				+ "| \u2665 \u2665 |\n"
+				+ "|____9|");
+		
+		System.out.println("\n--------------\n");
+		
+		
+		System.out.println(
+				  "SEVEN OF HEARTS\n"
+				+ " _____\n"
+				+ "|7    |\n"
+				+ "| \u2665 \u2665 |\n"
+				+ "|\u2665 \u2665 \u2665|\n"
+				+ "| \u2665 \u2665 |\n"
+				+ "|____L|");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "EIGHT OF HEARTS\n"
+				+ " _____\n"
+				+ "|8    |\n"
+				+ "|\u2665 \u2665 \u2665|\n"
+				+ "| \u2665 \u2665 |\n"
+				+ "|\u2665 \u2665 \u2665|\n"
+				+ "|____8|");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "NINE OF HEARTS\n"
+				+ " _____\n"
+				+ "|9    |\n"
+				+ "|\u2665 \u2665 \u2665|\n"
+				+ "|\u2665 \u2665 \u2665|\n"
+				+ "|\u2665 \u2665 \u2665|\n"
+				+ "|____6|");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "TEN OF HEARTS\n"
+				+ " _____\n"
+				+ "|10 \u2665 |\n"
+				+ "|\u2665 \u2665 \u2665|\n"
+				+ "|\u2665 \u2665 \u2665|\n"
+				+ "|\u2665 \u2665 \u2665|\n"
+				+ "|____0I|");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "JACK OF HEARTS\n"
+				+ " _____\n"
+				+ "|J    |\n"
+				+ "|     |\n"
+				+ "|  \u2665  |\n"
+				+ "|     |\n"
+				+ "|_____| ");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "QUEEN OF HEARTS\n"
+				+ " _____\n"
+				+ "|Q    |\n"
+				+ "|     |\n"
+				+ "|  \u2665  |\n"
+				+ "|     |\n"
+				+ "|_____| ");
+		
+		System.out.println("\n--------------\n");
+
+		
+		System.out.println(
+				  "KING OF HEARTS\n"
+				+ " _____\n"
+				+ "|K    |\n"
+				+ "|     |\n"
+				+ "|  \u2665  |\n"
+				+ "|     |\n"
+				+ "|_____| ");
+		
+		System.out.println("\n-----------------------------------------------------------------\n");
+	}
+
+}
