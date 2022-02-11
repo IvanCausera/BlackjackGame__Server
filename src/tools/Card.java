dc8c8b9678a2e7dbb07feebcb5480be0693decaa
alumno <alumno@PC13>
2022-02-11 13:54:42 +0100

    Atributos anyadidos a la clase Card

--- src/tools/Card.java
+++ src/tools/Card.java
@@ -4,11 +4,13 @@ public class Card {
 	private int id;
 	private String name;
 	private int score;
+	private String depiction;
 
-	public Card(int id, String name, int score) {
+	public Card(int id, String name, int score, String depiction) {
 		this.id = id;
 		this.name = name;
 		this.score = score;
+		this.depiction = depiction;
 	}
 
 	public int getId() {
@@ -34,6 +36,13 @@ public class Card {
 	public void setScore(int score) {
 		this.score = score;
 	}
-	
+
+	public String getDepiction() {
+		return depiction;
+	}
+
+	public void setDepiction(String depiction) {
+		this.depiction = depiction;
+	}
 	
 }
